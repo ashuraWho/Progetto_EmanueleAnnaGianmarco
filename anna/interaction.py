502a05f5eb19ad1528e349835d4f345e081fc68b
annafirinu <anna.firinu2@outlook.it>
2025-12-16 16:08:41 +0100

    interaction
    interaction

---
+++ anna/interaction.py
@@ -0,0 +1,61 @@
+import pandas as pd
+
+# Importo le stesse feature usate nella Fase A
+FEATURE_COLUMNS = [
+    "danceability", "energy", "speechiness", "acousticness",
+    "instrumentalness", "liveness", "valence", "tempo", "loudness"
+]
+
+# Creo la funzione interaction_step che mostra la canzone suggerita, chiede il voto reale all'utente, aggiorna user_history e seen_tracks
+#(song:canzone consigliata, confidence:probabilità che piaccia,user_history:DataFrame con lo storico utente, seen_tracks: set con track_id già ascoltati)
+
+def interaction_step(
+    song: pd.Series,
+    confidence: float,
+    user_history: pd.DataFrame,
+    seen_tracks: set
+):
+
+    # Stampa della canzone consigliata
+    print("\nAI DJ")
+    print(
+        f"Secondo i miei calcoli, questa ti piacerà al "
+        f"{confidence * 100:.0f}%:"
+    )
+    print(f"Ho scelto per te: {song['track_name']} - {song['artists']}")
+
+   
+    # Risposta dell'utente su quanto la canzone sia effettivamente azzeccata
+    while True:
+        vote = input("Ti è piaciuta? (1 = Sì, 0 = No): ")
+        if vote in ["0", "1"]:
+            vote = int(vote)
+            break
+        print("Inserisci solo 0 o 1")
+
+    # Creo una nuova riga da aggiungere a user_history
+    new_entry = {
+        "track_id": song["track_id"],
+        "track_name": song["track_name"],
+        "artists": song["artists"],
+        "vote": vote
+    }
+
+    # Aggiungo le feature numeriche
+    for feature in FEATURE_COLUMNS:
+        new_entry[feature] = song[feature]
+
+    # Append al DataFrame
+    user_history = pd.concat(
+        [user_history, pd.DataFrame([new_entry])],
+        ignore_index=True
+    )
+
+    # Segno la canzone come già ascoltata
+    seen_tracks.add(song["track_id"])
+
+    print("Fantastico! Ho imparato qualcosa di nuovo")
+    print("Sto ricalcolando il prossimo brano...")
+
+    # Restituisco tutto per il loop successivo
+    return user_history, seen_tracks
